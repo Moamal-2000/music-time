e519f3ba5b110e7ba0de993e472e02efc73876cb
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2023-04-06 07:26:34 +0300

    stopped music

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 'use strict'
 
-const musicFile = new Audio('music.mp3')
-const cassette = document.querySelector('.cassette')
+// const musicFile = new Audio('music.mp3')
+// const cassette = document.querySelector('.cassette')
 
 
-musicFile.volume = 0.3
+// musicFile.volume = 0.3
 
 
-function clickEffect(ele) {
-  ele.style.transform = "scale(0.994)"
-  setTimeout(() => ele.style.transform = "", 200);
-}
+// function clickEffect(ele) {
+//   ele.style.transform = "scale(0.994)"
+//   setTimeout(() => ele.style.transform = "", 200);
+// }
 
 
-cassette.onclick = () => {
-  musicFile.paused ? musicFile.play() : musicFile.pause()
-  clickEffect(cassette)
-}
+// cassette.onclick = () => {
+//   musicFile.paused ? musicFile.play() : musicFile.pause()
+//   clickEffect(cassette)
+// }
 
